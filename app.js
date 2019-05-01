833daf5e8e78b3c1d5f999a2eba84091cfe0bda8
phu26 <44081554+phu26@users.noreply.github.com>
2019-05-01 23:33:49 +0700

    update category
    load thanh category và số lượng các products của từng loại

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.use(bodyParser.urlencoded({
     extended: false
 }));
 app.use(bodyParser.json());
+app.use((require('./middlewares/category.mdw') ) );
 app.use('/categories', require('./routes/categories'));
 app.get('/', (req, res) => {
     catogoryModel.all()
--- models/category.model.js
+++ models/category.model.js
@@ -4,6 +4,14 @@ module.exports = {
             
         return db.load('select * from categories');
     },
+    single: id => {
+            
+        return db.load(`select * from categories where CatID = ${id}`);
+    },
+    allWithDetails: () => {
+            
+        return db.load(`select c.*, count(p.ProID) as num_of_products from categories c left join products p on c.CatID = p.CatID group by c.CatID,c.CatName`);
+    },
     add: entity => {
         return db.add('categories',entity);
     }
