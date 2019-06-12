edd89a293be83d58e4df51555de0e407418e0478
phu26 <44081554+phu26@users.noreply.github.com>
2019-06-12 13:07:38 +0700

    upload ảnh lên ckeditor thành công

--- routes/products.js
+++ routes/products.js
@@ -3,6 +3,22 @@ var productModel = require('../models/product.model');
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var fs = require('fs')
+var path = require('path')
+var crypto = require('crypto');
+var bodyParser = require('body-parser');
+var multer  =   require('multer');
+
+var storage = multer.diskStorage({
+  destination: 'public/pic/',
+  filename: function (req, file, cb) {
+    crypto.pseudoRandomBytes(16, function (err, raw) {
+      if (err) return cb(err)
+      cb(null, Math.floor(Math.random()*9000000000) + 1000000000 + path.extname(file.originalname))
+    })
+  }
+})
+var upload = multer({ storage: storage });
 
 io.on('connection', function (socket) {
   console.log('a user connected');
@@ -41,7 +57,7 @@ var relatee = function (req, res, next) {
     .catch(err => next(err));
 }
 
-router.get('/:id', [cmt, relatee], function (req, res, next) {
+router.get('/sp/:id', [cmt, relatee], function (req, res, next) {
   var id = req.params.id;
   console.log(id);
   if (id == '') {
@@ -115,7 +131,7 @@ var cmt2 = function (req, res, next) {
 }
 
 
-router.post('/:id', [b, cmt2,relatee], function (req, res, next) {
+router.post('/sp/:id', [b, cmt2,relatee], function (req, res, next) {
   var id = req.params.id;
 
   productModel.AddCmt(req.body.comment, req.cmt.f_ID, id);
@@ -153,5 +169,24 @@ router.post('/',(req,res) =>{
   console.log(req.body.content);
   res.end(req.body.content);
 })
-
+router.get('/files', function (req, res) {
+  const images = fs.readdirSync('public/pic')
+  var sorted = []
+  for (let item of images){
+      if(item.split('.').pop() === 'png'
+      || item.split('.').pop() === 'jpg'
+      || item.split('.').pop() === 'jpeg'
+      || item.split('.').pop() === 'svg'){
+          var abc = {
+                "image" : "/pic/"+item,
+                "folder" : '/'
+          }
+          sorted.push(abc)
+      }
+  }
+  res.send(sorted);
+})
+router.post('/', upload.array('flFileUpload', 12), function (req, res, next) {
+  res.redirect('back');
+});
 module.exports = router;
